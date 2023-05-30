c30374ef8e150a0a6f85a1284c24eb9fc64efc79
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-30 15:16:27 +0600

    Create a class called MusicAlbum and add properties inside the constructor

---
+++ music.rb
@@ -0,0 +1,15 @@
+equire_relative 'item'
+require 'date'
+
+class MusicAlbum < Item
+  attr_accessor :name, :genres, :publish_date, :on_spotify
+  attr_writer :genre
+
+  def initialize(name, publish_date, on_spotify: false, genres: [])
+    super()
+    @name = name
+    @publish_date = Date.parse(publish_date)
+    @on_spotify = on_spotify
+    @genres = genres
+  end
+end
