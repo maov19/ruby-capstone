61f611844a7f69b63df1d199d30099a1a64b8af1
mossa <martin.ossa.8@gmail.com>
2023-05-30 19:02:28 +0200

    Update book to implement methods

--- book.rb
+++ book.rb
@@ -2,15 +2,21 @@ require_relative 'item'
 require 'date'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state, :label
+  attr_writer :label
 
-  def initialize(publish_date, publisher, cover_state)
+  def initialize(publish_date, publisher, cover_state, labels: [])
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+    @labels = labels
   end
 
   def can_be_archived?
     super || @cover_state.downcase == 'bad'
+  end
+
+  def add_label
+    @labels << label
   end
 end
