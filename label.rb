4073ac6e7fe736e875be201af95d7b5cf4e5794e
mossa <martin.ossa.8@gmail.com>
2023-05-30 12:09:26 +0200

    Fix styling issue

--- label.rb
+++ label.rb
@@ -1,1 +1,15 @@
-label.rb
+class Label
+    attr_accessor :id, :title, :color, :items
+  
+    def initialize(id, title, color)
+      @id = id
+      @title = title
+      @color = color
+      @items = []
+    end
+  
+    def add_item(item)
+      @items << item
+    end
+  end
+  
