3f59fbadf22ad177da848feb822e0fae064ca231
mossa <martin.ossa.8@gmail.com>
2023-05-30 13:38:15 +0200

    Update archive method

--- book.rb
+++ book.rb
@@ -1,3 +1,6 @@
+require_relative 'item'
+require 'date'
+
 class Book < Item
   attr_accessor :publisher, :cover_state
 
@@ -8,6 +11,6 @@ class Book < Item
   end
 
   def can_be_archived?
-    super && @cover_state == 'Good'
+    super || @cover_state.downcase == 'bad'
   end
 end
