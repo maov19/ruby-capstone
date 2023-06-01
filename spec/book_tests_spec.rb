17b09ffaf78d64c8c8d419ed335f06180fc188a5
mossa <martin.ossa.8@gmail.com>
2023-06-01 16:50:11 +0200

    Fix unit tests for book methods

--- spec/book_tests_spec.rb
+++ spec/book_tests_spec.rb
@@ -2,12 +2,10 @@ require_relative '../book_app'
 require_relative '../book'
 require_relative '../label'
 
-RSpec.describe BookActions do
+RSpec.describe Book do
   describe '#can_be_archived?' do
-    let(:book_actions) { BookActions.new }
-
     context 'when cover state is "good" and publish date is 5 years ago' do
-      it 'returns true' do
+      it 'returns false' do
         book = Book.new('Test Book', Date.today - (5 * 365), 'Test Publisher', 'good')
         expect(book.can_be_archived?).to be(false)
       end
@@ -21,3 +19,4 @@ RSpec.describe BookActions do
     end
   end
 end
+
