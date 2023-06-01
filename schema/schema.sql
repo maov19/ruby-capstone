e97ddf8eae81b4d845a13f998cfdf88d22589675
mossa <martin.ossa.8@gmail.com>
2023-06-01 10:47:32 +0200

    Add book and label to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,36 @@
 --Create database
 CREATE DATABASE my_catalog;
 
+-- Create item table
+CREATE TABLE item (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (label_id) REFERENCES label(id)
+);
+
+-- Create label table
+CREATE TABLE label (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(100),
+  color VARCHAR(100),
+  items INT[]
+);
+
+-- Create book table (specialized form of item)
+CREATE TABLE book (
+  item_id INT PRIMARY KEY,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100),
+  FOREIGN KEY (item_id) REFERENCES item(id)
+);
+
+
 -- Create genres table
 CREATE TABLE genres (
   id INT Generated ALWAYS AS IDENTITY PRIMARY KEY,
